1479a5e724b8cede2b60e65566a6feab9e2d86d2
Vladimir <vzh.inbox@gmail.com>
2019-04-24 18:38:02 +0600

    Update deps and cofigure

---
+++ .prettierrc.js
@@ -0,0 +1,1 @@
+module.exports = require('@vzh/prettier-config');
---
+++ babel.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: '@vzh/configs/babel/common.babelrc.json',
+  plugins: ['@babel/plugin-proposal-class-properties'],
+};

1cf0da5c303e803c801ae02fd587c3f0d5fcf8ce
Vladimir <vzh.inbox@gmail.com>
2020-05-22 13:19:00 +0600

    Update deps and fix

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: '@vzh/configs/babel/common.babelrc.js',
+  extends: '@vzh/configs/babel/env.babelrc.js',
   plugins: [
     '@babel/plugin-proposal-class-properties',
     'babel-plugin-transform-inline-environment-variables',

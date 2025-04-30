6a81a86c85cfe37e8245e7d2efa3321dd69a4111
Vladimir <vzh.inbox@gmail.com>
2025-04-30 17:04:37 +0500

    Update configs

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
+/** @type {import('@babel/core').TransformOptions} */
 module.exports = {
   extends: '@js-toolkit/configs/babel/env.babelrc.js',
   plugins: [
     '@babel/plugin-proposal-class-properties',
     'babel-plugin-transform-inline-environment-variables',
   ],
+  assumptions: {
+    enumerableModuleMeta: true,
+  },
 };
